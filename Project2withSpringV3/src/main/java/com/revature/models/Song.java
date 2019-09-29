adcfdd586b47381a7db2fbc142911ad3e23c6fc5
Fred Glaeser IV <fredjg4@gmail.com>
2019-09-28 23:08:57 -0400

    add song and artist and i forget rest of features

--- Project2withSpringV3/src/main/java/com/revature/models/Song.java
+++ Project2withSpringV3/src/main/java/com/revature/models/Song.java
@@ -23,8 +23,14 @@ public class Song implements Serializable {
 	@Column (name = "song_id")
 	private Integer songId;
 	
+	private String name;
+	
+	private String artist;
+	
 	@Column(length=50)
 	private String spotifySongId;
+	
+	
 	
 	public Song() {
 		super();
@@ -40,15 +46,26 @@ public class Song implements Serializable {
 		this.spotifySongId = spotifySongId;
 	}
 
+	public Song(Integer songId, String spotifySongId, String name, String artist) {
+		super();
+		this.songId = songId;
+		this.spotifySongId = spotifySongId;
+		this.name = name;
+		this.artist = artist;
+	}
+
 	@Override
 	public String toString() {
-		return "Song [songId=" + songId + ", spotifySongId=" + spotifySongId + "]";
+		return "Song [songId=" + songId + ", spotifySongId=" + spotifySongId + ", name=" + name + ", artist=" + artist
+				+ "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((artist == null) ? 0 : artist.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((songId == null) ? 0 : songId.hashCode());
 		result = prime * result + ((spotifySongId == null) ? 0 : spotifySongId.hashCode());
 		return result;
@@ -63,6 +80,16 @@ public class Song implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Song other = (Song) obj;
+		if (artist == null) {
+			if (other.artist != null)
+				return false;
+		} else if (!artist.equals(other.artist))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		if (songId == null) {
 			if (other.songId != null)
 				return false;
@@ -92,7 +119,21 @@ public class Song implements Serializable {
 		this.spotifySongId = spotifySongId;
 	}
 
-	
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
 	
 }
